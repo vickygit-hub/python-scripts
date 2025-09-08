407c9dd737526cfe2e8278763db941a1c671d704
vikas singh kushwaha <vicky.singh.250803@gmail.com>
2025-09-08 08:41:58 +0000

    added mymodules and main successfully

---
+++ main.py
@@ -0,0 +1,4 @@
+# main.py
+import mymodule   # importing your custom module
+
+print(mymodule.greet("Vicky"))   # call function from module
---
+++ mymodule.py
@@ -0,0 +1,3 @@
+# mymodule.py
+def greet(name):
+    return f"Hello, {name}! Welcome to DevOps learning."
